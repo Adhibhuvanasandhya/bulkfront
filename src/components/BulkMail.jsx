064ad916edd5159e924b8c7c7a62b9134a9f8ba8
Adhibhuvanasandhya <sandhyaponrajan.l@gmail.com>
2024-12-22 11:54:40 +0530

    updated

--- src/components/BulkMail.jsx
+++ src/components/BulkMail.jsx
@@ -38,10 +38,10 @@ function BulkMail() {
     axios
       .post("https://bulkback.onrender.com/sendemail", { msg, emailList })
       .then((response) => {
-        alert( "Emails sent successfully!");
+        alert(response.data.message || "Emails sent successfully!");
       })
       .catch((err) => {
-        alert("Error: Failed to send emails.");
+        alert(`Error: ${err.response?.data?.message || "Failed to send emails."}`);
       })
       .finally(() => {
         setstatus(false);
